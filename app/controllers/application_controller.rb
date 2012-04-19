9fdbabe6c86c1336b516442f3093d4c2c066ecf3
Petre Diaconescu <petrero@gmail.com>
2012-04-19 10:33:28 +0300

    making presenter accessible from controller layer"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  private
+     def present(object, klass = nil)
+        klass ||= "#{object.class}Presenter".constantize
+        klass.new(object, view_context)
+     end
 end
