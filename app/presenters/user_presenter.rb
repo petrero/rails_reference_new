e2fdfb1acca4d58f9ed3cfd9a3b8c8016babe68e
Petre Diaconescu <petrero@gmail.com>
2012-04-19 09:38:35 +0300

    sharing common functionality between presenters

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -1,21 +1,14 @@
-class UserPresenter
-  def initialize(user, template)
-    @user = user
-    @template = template
-  end
-  
-  def h
-    @template
-  end
+class UserPresenter < BasePresenter
+  presents :user
   
   def avatar
-    h.link_to_if @user.url.present?, h.image_tag("avatars/#{avatar_name}", class: "avatar"), @user.url
+    h.link_to_if user.url.present?, h.image_tag("avatars/#{avatar_name}", class: "avatar"), user.url
   end
   
   private
     def avatar_name
-      if @user.avatar_image_name.present?
-        @user.avatar_image_name
+      if user.avatar_image_name.present?
+        user.avatar_image_name
       else
         "default.png"
       end
