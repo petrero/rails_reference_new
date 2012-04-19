401a0fa1d27431d9546ac9c0405f63528de787ee
Petre Diaconescu <petrero@gmail.com>
2012-04-19 14:41:03 +0300

    treating the case when response is not structured as we expect

--- app/models/zip_code.rb
+++ app/models/zip_code.rb
@@ -3,11 +3,15 @@ class ZipCode
   
   def initialize(zip)
     client = Savon::Client.new("http://webservicex.net/uszip.asmx?WSDL")
-    response = client.request :web, :get_info_by_zip, body: {"USZip" => "90210"}
-    data = response.to_hash[:get_info_by_zip_response][:get_info_by_zip_result][:new_data_set][:table]
-    @state = data[:state]
-    @city = data[:city]
-    @area_code = data[:area_code]
-    @time_zone = data[:time_zone] 
+    response = client.request :web, :get_info_by_zip, body: {"USZip" => zip}
+    if response.success?
+      data = response.to_array(:get_info_by_zip_response, :get_info_by_zip_result, :new_data_set, :table).first
+      if data
+        @state = data[:state]
+        @city = data[:city]
+        @area_code = data[:area_code]
+        @time_zone = data[:time_zone] 
+      end
+    end
   end
 end
