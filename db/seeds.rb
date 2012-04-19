4960169a2cd6fc0243ca79aca9af1247860569b0
Petre Diaconescu <petrero@gmail.com>
2012-04-19 09:08:17 +0300

    Initial user application

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(
+  username: "petrero",
+  full_name: "Diaconescu Petrisor",
+  twitter_name: "petrero",
+  avatar_image_name: "batman.jpg",
+  url: "http://railscasts.com/",
+  bio: <<-BIO
+I also work on the following projects.
+
+* [Ticketee](http://github.com/ticketee)
+* [Rails Reference](http://github.com/rails-reference)
+* [Go vs Go](http://govsgo.com/)
+
+Be sure to check out my [GitHub page](https://github.com/ryanb) for more projects.
+BIO
+)
+
+User.create(username: "MrMystery")
