c68a1fc929424b1091305c002d2960def75b77ea
Petre Diaconescu <petrero@gmail.com>
2012-04-19 18:42:14 +0300

    disabling logging output test in rake rspec console

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,10 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/mechanize'
 
+HTTPI.log = false
+Savon.log = false
+
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
