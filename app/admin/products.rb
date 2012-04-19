b1f698ebc997246f4a3c8f512cbbefcaaacc9ae2
Petre Diaconescu <petrero@gmail.com>
2012-04-19 11:53:34 +0300

    customizing how the products admin view looks

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register Product do
+  scope :unreleased
   
+  index do
+    column :name
+    column :category
+    column "Released Date", :released_at
+    column :price, :sortable => :price do |product|
+      div class: "price" do #align price to right
+        number_to_currency product.price
+      end
+    end
+    default_actions
+  end
 end
