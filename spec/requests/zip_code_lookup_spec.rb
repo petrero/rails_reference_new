e182f6195b92a41d391ce15585f55b0bd111198a
Petre Diaconescu <petrero@gmail.com>
2012-04-19 18:07:10 +0300

    set up vcr in Rspec.configure hash

--- spec/requests/zip_code_lookup_spec.rb
+++ spec/requests/zip_code_lookup_spec.rb
@@ -1,12 +1,10 @@
 require "spec_helper"
 
 describe "ZipCodeLookup" do
-  it "show Beverly Hills given 90210" do
-    VCR.use_cassette "zip_code/90210" do
-      visit root_path
-      fill_in "zip_code", with: "90210"
-      click_on "Lookup"
-      page.should have_content("Beverly Hills")
-    end
+  it "show Beverly Hills given 90210", :vcr do  
+     visit root_path
+     fill_in "zip_code", with: "90210"
+     click_on "Lookup"
+     page.should have_content("Beverly Hills")
   end
 end
