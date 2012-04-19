54e49b468fe1e0bd3355680d6ac647b6d1e885e9
Petre Diaconescu <petrero@gmail.com>
2012-04-19 10:10:12 +0300

    moving all complex logic from show view into user_presenter

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -2,15 +2,58 @@ class UserPresenter < BasePresenter
   presents :user
   
   def avatar
-    h.link_to_if user.url.present?, h.image_tag("avatars/#{avatar_name}", class: "avatar"), user.url
+    site_link h.image_tag("avatars/#{avatar_name}", class: "avatar")
+  end
+  
+  def linked_name
+    site_link(user.full_name.present? ? user.full_name : user.username)
+  end
+  
+  def member_since
+    user.created_at.strftime("%B %e, %Y")
+  end
+  
+  def website
+    handle_none(user.url) do
+      h.link_to user.url, user.url
+    end
+  end
+  
+  def twitter
+    handle_none(user.twitter_name) do
+      h.link_to user.twitter_name, "http://twitter.com/#{user.twitter_name}"
+    end
+  end
+  
+  def bio
+    handle_none(user.bio) do
+      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :hard_wrap => true, :filter_html => true, :autolink => true)
+      markdown.render(user.bio).html_safe
+    end
+  end
+  
+  def username
+    user.username
   end
   
   private
+    def site_link(content)
+      h.link_to_if user.url.present?, content, user.url
+    end
+    
     def avatar_name
       if user.avatar_image_name.present?
         user.avatar_image_name
       else
         "default.png"
+      end
+    end
+    
+    def handle_none(value)
+      if value.present?
+        yield
+      else
+        h.content_tag 'span', 'None given', class: "none"  
       end
     end
 end
