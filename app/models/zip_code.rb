d1408ef881ecfae821bf68df176b96d9b489a851
Petre Diaconescu <petrero@gmail.com>
2012-04-19 14:17:04 +0300

    making lookup by zip to function

--- app/models/zip_code.rb
+++ app/models/zip_code.rb
@@ -2,5 +2,12 @@ class ZipCode
   attr_reader :state, :city, :area_code, :time_zone
   
   def initialize(zip)
+    client = Savon::Client.new("http://webservicex.net/uszip.asmx?WSDL")
+    response = client.request :web, :get_info_by_zip, body: {"USZip" => "90210"}
+    data = response.to_hash[:get_info_by_zip_response][:get_info_by_zip_result][:new_data_set][:table]
+    @state = data[:state]
+    @city = data[:city]
+    @area_code = data[:area_code]
+    @time_zone = data[:time_zone] 
   end
 end
