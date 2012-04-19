d7bc70a00eeea00d59b22c06402493f7410607eb
Petre Diaconescu <petrero@gmail.com>
2012-04-19 10:52:36 +0300

    presenter doesn't have access to methods defined at controller level

--- spec/presenters/user_presenter_spec.rb
+++ spec/presenters/user_presenter_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe UserPresenter do
   
   it "says when none given" do
+    #view.current_user {it will say it's undefined because is defined at controller level and view doesn't have access to it; must be stubbed out}
     presenter = UserPresenter.new(User.new, view)
     presenter.website.should include("None given")
   end
