e553b0b8c032b32f881e6ad8b524efe3ac35ba71
Petre Diaconescu <petrero@gmail.com>
2012-04-19 16:41:15 +0300

    add cassete around our spec test

--- spec/requests/zip_code_lookup_spec.rb
+++ spec/requests/zip_code_lookup_spec.rb
@@ -2,9 +2,11 @@ require "spec_helper"
 
 describe "ZipCodeLookup" do
   it "show Beverly Hills given 90210" do
-    visit root_path
-    fill_in "zip_code", with: "90210"
-    click_on "Lookup"
-    page.should have_content("Beverly Hills")
+    VCR.use_cassette "zip_code/90210" do
+      visit root_path
+      fill_in "zip_code", with: "90210"
+      click_on "Lookup"
+      page.should have_content("Beverly Hills")
+    end
   end
 end
