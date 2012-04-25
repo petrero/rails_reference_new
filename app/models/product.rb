0b60dc57207a6453a7858761f4877d261e9ea2e4
Petre Diaconescu <petrero@gmail.com>
2012-04-25 18:11:48 +0300

    passing the product formated on the client side into output rendered on the server side

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  def for_mustache
+    context = V8::Context.new
+    coffee = File.read(Rails.root.join("app/assets/javascripts/product.js.coffee"))
+    javascript = CoffeeScript.compile(coffee)
+    context.eval(javascript)
+    context.eval("new Product(#{to_json})")
+  end
 end
