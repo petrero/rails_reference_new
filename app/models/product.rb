090d92ddc6a3ac56712c6a5db0323d47418a6fe2
Petre Diaconescu <petrero@gmail.com>
2012-04-25 18:43:48 +0300

    cache the context object (the context object will never be garbbaged collected so be aware of possible code memory leaks, the safe way is memcached)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,10 @@
 class Product < ActiveRecord::Base
   def for_mustache
-    context = V8::Context.new
-    coffee = File.read(Rails.root.join("app/assets/javascripts/product.js.coffee"))
-    javascript = CoffeeScript.compile(coffee)
-    context.eval(javascript)
-    context.eval("new Product(#{to_json})")
+    Thread.current[:product_v8] ||= V8::Context.new.tap do |context|
+      coffee = File.read(Rails.root.join("app/assets/javascripts/product.js.coffee"))
+      javascript = CoffeeScript.compile(coffee)
+      context.eval(javascript)
+    end
+    Thread.current[:product_v8].eval("new Product(#{to_json})")
   end
 end
