8eb3218a6655cbd7fb18483afc77d7dc8e4b8995
Petre Diaconescu <petrero@gmail.com>
2012-04-26 09:29:55 +0300

    Initial Users Crud

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create! name: "Batman", email: "batman@example.com", gender: "Male", public_profile: true, bio: <<-BIO
+The Dark Knight is dedicated to ridding Gotham of evil. Complete with his cape, utility belt, and bullet proof suite, no criminal has a chance against this Caped Crusader.
+BIO
