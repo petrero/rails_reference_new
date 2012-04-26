0a1d3ffc3c5802bfd2774363a6a1fc0c632fc64f
Petre Diaconescu <petrero@gmail.com>
2012-04-26 14:44:13 +0300

    using warden outside user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 RailsReferenceNew::Application.routes.draw do
-  get "signup", to: "users#new", as: "signup"
-  get "login", to: "sessions#new", as: "login"
+  scope constraints: lambda { |r| r.env['warden'].user.nil? } do
+    get "signup", to: "users#new", as: "signup"
+    get "login", to: "sessions#new", as: "login"
+  end
   get "logout", to: "sessions#destroy", as: "logout"
   root to: "home#index"
   resources :users
