bbd60dda5977e939d5998962044bc2ea1c7c5861
Petre Diaconescu <petrero@gmail.com>
2012-04-26 10:27:44 +0300

    modifying users controller so the user errors full_messages to be displayed when an error email validation ocurr

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  respond_to :html, :json
   def index
     @users = User.all
   end
@@ -27,12 +26,20 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update_attributes(params[:user])
-    respond_with @user
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to(@user, :notice => 'User was successfully updayed.') }
+        format.json { head :ok }
+      else
+        format.html { render :action => "edit"}
+        format.json { render :json => @user.errors.full_messages, :status => :unprocessable_entity }
+      end
+    end
+    
   end
 
   def destroy
-    @user = User.find(params[:id])
+    @user = User.find(params[:id])   
     @user.destroy
     redirect_to users_url, notice: "Successfully destroyed user."
   end
