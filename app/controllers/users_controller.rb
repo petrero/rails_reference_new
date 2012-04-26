7da5d1e5aa49ba91d9236e00a30bd0e13c126b22
Petre Diaconescu <petrero@gmail.com>
2012-04-26 14:25:47 +0300

    using warden in users controller too

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      session[:user_id] = @user.id
+      warden.set_user(@user)
       redirect_to root_url, notice: "Signed up!"
     else
       render "new"
