b771723e9993aab50b4e12c01c2ee0e2afe58e9f
Petre Diaconescu <petrero@gmail.com>
2012-04-26 14:22:46 +0300

    resolving finding proper current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    warden.user
   end
   helper_method :current_user
+  
+  def warden
+    env['warden']
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = env['warden'].authenticate
+    user = warden.authenticate
     if user
       redirect_to root_url, notice: "Logged in!"
     else
-      flash.now.alert = env['warden'].message
+      flash.now.alert = warden.message
       render "new"
     end
   end
 
   def destroy
-    env['warden'].logout
+    warden.logout
     redirect_to root_url, notice: "Logged out!"
   end
 end
