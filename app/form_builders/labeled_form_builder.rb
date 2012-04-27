5041107802572ecc3963de32a7c7068b4dffe1fc
Petre Diaconescu <petrero@gmail.com>
2012-04-27 13:36:35 +0300

    overriding(customizing) text_field text_area password_field collection_select methods from Helpers::FormBuilder

--- app/form_builders/labeled_form_builder.rb
+++ app/form_builders/labeled_form_builder.rb
@@ -1,7 +1,10 @@
 class LabeledFormBuilder < ActionView::Helpers::FormBuilder
-  def text_field(name, *args)
-    @template.content_tag :div, class: "field" do
-      label(name) + @template.tag(:br) + super  
-    end 
-  end  
+  
+  %w[text_field text_area password_field collection_select].each do |method_name|
+    define_method(method_name) do |name, *args|
+      @template.content_tag :div, class: "field" do
+        label(name) + @template.tag(:br) + super(name, *args)  
+      end   
+    end
+  end
 end
