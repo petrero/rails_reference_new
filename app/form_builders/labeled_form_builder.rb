56d23d3e7b0952328f7ccc6d27809498acf35f72
Petre Diaconescu <petrero@gmail.com>
2012-04-27 13:52:22 +0300

    customizing label text in our customized helper methods problem: input price field has an unwanted label option"

--- app/form_builders/labeled_form_builder.rb
+++ app/form_builders/labeled_form_builder.rb
@@ -3,14 +3,20 @@ class LabeledFormBuilder < ActionView::Helpers::FormBuilder
   %w[text_field text_area password_field collection_select].each do |method_name|
     define_method(method_name) do |name, *args|
       @template.content_tag :div, class: "field" do
-        label(name) + @template.tag(:br) + super(name, *args)  
+        field_label(name, *args) + @template.tag(:br) + super(name, *args)  
       end   
     end
   end
   
   def check_box(name, *args)
     @template.content_tag :div, class: "field" do
-      super + " " + label(name)
+      super + " " + field_label(name, *args)
     end
   end
+  
+  private
+    def field_label(name, *args)
+      options = args.extract_options!
+      label(name, options[:label])
+    end
 end
