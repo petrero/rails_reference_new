ce39de86f337c355d7e2d5928836d2995a744e2e
Petre Diaconescu <petrero@gmail.com>
2012-04-27 08:44:54 +0300

    changing default behaviour to searching with AND operator instead of OR

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,7 +7,7 @@ class Article < ActiveRecord::Base
   
   def self.search(params)
     tire.search(load: true) do
-      query { string params[:query] } if params[:query].present?
+      query { string params[:query], default_operator: "AND" } if params[:query].present?
       filter :range, published_at: {lte: Time.zone.now }
     end
   end
