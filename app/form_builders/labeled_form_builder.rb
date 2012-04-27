bc84db60c46ffcf724ea1a96a29da36b07b89e0c
Petre Diaconescu <petrero@gmail.com>
2012-04-27 14:19:53 +0300

    delegating content_tag and tag methods to @template

--- app/form_builders/labeled_form_builder.rb
+++ app/form_builders/labeled_form_builder.rb
@@ -1,26 +1,27 @@
 class LabeledFormBuilder < ActionView::Helpers::FormBuilder
+  delegate :content_tag, :tag, to: :@template
   
   %w[text_field text_area password_field collection_select].each do |method_name|
     define_method(method_name) do |name, *args|
-      @template.content_tag :div, class: "field" do
-        field_label(name, *args) + @template.tag(:br) + super(name, *args)  
+      content_tag :div, class: "field" do
+        field_label(name, *args) + tag(:br) + super(name, *args)  
       end   
     end
   end
   
   def check_box(name, *args)
-    @template.content_tag :div, class: "field" do
+    content_tag :div, class: "field" do
       super + " " + field_label(name, *args)
     end
   end
   
   def error_messages
     if object.errors.full_messages.any?
-      @template.content_tag(:div, :class=> "error_messages") do
-        @template.content_tag(:h2, "Invalid Fields") +
-        @template.content_tag(:ul) do      
+      content_tag(:div, :class=> "error_messages") do
+        content_tag(:h2, "Invalid Fields") +
+        content_tag(:ul) do      
           object.errors.full_messages.map do |msg|
-            @template.content_tag(:li, msg)
+            content_tag(:li, msg)
           end.join.html_safe
         end
       end
