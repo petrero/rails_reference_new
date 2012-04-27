be3a7d6597da7e5b5995c232860d8b4bd972b169
Petre Diaconescu <petrero@gmail.com>
2012-04-27 09:14:44 +0300

    fetching the searched records from index instead rely on the sql query(index all the necessary attributes we need to display the articles)

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,7 +6,7 @@ class Article < ActiveRecord::Base
   include Tire::Model::Callbacks
   
   def self.search(params)
-    tire.search(load: true, page: params[:page], per_page: 2) do
+    tire.search(page: params[:page], per_page: 2) do
       query { string params[:query], default_operator: "AND" } if params[:query].present?
       filter :range, published_at: {lte: Time.zone.now }
       sort {by :published_at, "desc" } if params[:query].blank?
@@ -14,10 +14,14 @@ class Article < ActiveRecord::Base
   end
   
   def to_indexed_json
-    to_json(methods: [:author_name])
+    to_json(methods: [:author_name, :comments_count])
   end
   
   def author_name
     author.name
+  end
+  
+  def comments_count
+    comments.size
   end
 end
